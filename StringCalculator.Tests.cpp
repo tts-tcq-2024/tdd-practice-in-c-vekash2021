1bad056e417c31d4c95064cdc80d5850e9e66771
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-07-09 10:39:24 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -8,12 +8,12 @@ TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
     ASSERT_EQ(result, expectedresult);
 }
 
-// TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
-//     int expectedresult = 0;
-//     const char* input = "0";
-//     int result = add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
+TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
+    int expectedresult = 0;
+    const char* input = "0";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
 
 // TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
 //     int expectedresult = 3;
