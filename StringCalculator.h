996dc7d0967f7de1f7e55a2cd3384c96bb5f5fe1
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-07-10 10:29:42 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,7 +1,7 @@
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
 
-int check_for_empty_string(const char* input_string);
-int add(const char* input_string) ;
+
+int add(const char *input_string) ;
 
 #endif // STRINGCALCULATOR_H
