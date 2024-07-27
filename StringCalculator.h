caba2dd15d035187295d47f95990bc2c9a66711c
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-07-27 13:01:09 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -10,25 +10,20 @@ int add_number_to_sum(int current_number, int sum) {
 int add(const char *input_string) {
     int sum = 0;
     int current_number = 0;
-    int in_number = 0;  // Flag to track if currently parsing a number
-    
+
     // Traverse through each character of the input string
     while (*input_string) {
         if (isdigit(*input_string)) {
             current_number = current_number * 10 + (*input_string - '0');
-            in_number = 1;
-        } else if (in_number) {
+        } else {
             sum = add_number_to_sum(current_number, sum);
             current_number = 0;
-            in_number = 0;
         }
         input_string++;
     }
-    
+
     // Add the last number if the string ends with a number <= 1000
-    if (in_number) {
-        sum = add_number_to_sum(current_number, sum);
-    }
+    sum = add_number_to_sum(current_number, sum);
     
     return sum;
 }
